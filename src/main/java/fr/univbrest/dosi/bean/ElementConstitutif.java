5d30c6193c97aea5a36cf333fd2780df1debb52f
mds <mds.haballa@gmail.com>
2019-03-21 14:26:52 +0100

    Sprint v1.0

--- src/main/java/fr/univbrest/dosi/bean/ElementConstitutif.java
+++ src/main/java/fr/univbrest/dosi/bean/ElementConstitutif.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
  * 
  */
 @Entity
-@Table(name="ELEMENT_CONSTITUTIF")
+@Table(name="element_constitutif")
 
 @NamedQueries({
 @NamedQuery(name="ElementConstitutif.findAll", query="SELECT e FROM ElementConstitutif e"),
